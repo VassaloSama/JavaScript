e2a25f9294a103f18f8507c5a2f0f03ed6b8b09e
LucasVassalo <lv.cristiano@hotmail.com>
2023-08-30 20:48:44 -0300

    continuação dos estudos

---
+++ Exercícios/moduloD/ex008.js
@@ -0,0 +1,6 @@
+var vel = 88
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if (vel > 60) { // Condição Simples
+    console.log(`Você ultrapassou a velocidade máxima permitida, MULTADO!`)
+}
+console.log(`Dirija sempre usando cinto de segurança!`)
