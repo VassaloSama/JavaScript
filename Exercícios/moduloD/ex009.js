de3cff9ebe268a14e1f453bbe78745236d04ad67
LucasVassalo <lv.cristiano@hotmail.com>
2023-09-05 00:07:48 -0300

    continuação dos estudos de JavaScript

--- Exercícios/moduloD/ex009.js
+++ Exercícios/moduloD/ex009.js
@@ -1,7 +1,7 @@
 var pais = 'França'
 console.log(`Vivendo em ${pais}`)
-if (pais == 'Brasil') {
+if (pais == 'Brasil') { // condição simples
     console.log('Você é Brasileiro!')
-}else {
+}else { // condição compostas
     console.log('Você é estrangeiro!')
 }
