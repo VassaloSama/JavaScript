d5c730cb2d53ef3d715e7057d98e9722bd879492
LucasVassalo <lv.cristiano@hotmail.com>
2023-09-13 22:29:22 -0300

    estudos de repetições JavaScript

---
+++ Exercícios/moduloE/ex014.js
@@ -0,0 +1,17 @@
+/*
+        Estrutura de repetição com teste Lógico no inicio
+var c = 1
+while (c <=10) {
+    console.log(`passo ${c}`)
+    c++
+}
+*/
+
+/*
+        Estrutura de repetição com teste lógico no final
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <=10)
+*/
---
+++ Exercícios/moduloE/ex015.js
@@ -0,0 +1,5 @@
+console.log('Vamos começar...')
+for (var c = 1;c <= 10;c++) {
+    console.log(c)
+}
+console.log('Fim!')
